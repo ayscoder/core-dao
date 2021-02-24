ae28d23f63f7cfb0e0fc1b482fba91b4bf888319
ayush <jbytrain@gmail.com>
2021-02-24 23:26:26 +0530

    Added application

---
+++ src/main/java/com/ays/DaoApplication.java
@@ -0,0 +1,19 @@
+package com.ays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DaoApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DaoApplication.class);
+    }
+}
